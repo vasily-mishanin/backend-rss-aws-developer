7ffa2bf2ca4a35da6fec60a5a0beef239daa63af
vasily <vasilymishanin@gmail.com>
2023-11-18 23:51:35 +0600

    feat: get products list response

---
+++ product-service/handler.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { products } = require('./productsMock.js');
+
+module.exports = {
+  getProductsList: async function (event) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products, null, 2),
+    };
+  },
+};
