b85de341ecaff758c728caef18a87247ce8b89aa
vasily <vasilymishanin@gmail.com>
2023-12-01 22:54:47 +0600

    construct: stack with lambda and api integration

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,68 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  ApiKey,
+  ApiKeySourceType,
+  Cors,
+  LambdaIntegration,
+  RestApi,
+  UsagePlan,
+} from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //API
+    const api = new RestApi(this, 'importRestAPI', {
+      restApiName: 'importRestAPI',
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+      apiKeySourceType: ApiKeySourceType.HEADER,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    const apiKey = new ApiKey(this, 'ApiKey');
+
+    const usagePlan = new UsagePlan(this, 'UsagePlan', {
+      name: 'UsagePlan',
+      apiStages: [
+        {
+          api,
+          stage: api.deploymentStage,
+        },
+      ],
+    });
+
+    usagePlan.addApiKey(apiKey);
+
+    // LAMBDAS
+    const importProductsFileLambda = new NodejsFunction(
+      this,
+      'importProductsFileLambda',
+      {
+        entry: 'resources/endpoints/import-products.ts',
+        handler: 'handler',
+        environment: {},
+      }
+    );
+
+    // LINK all together
+    // Integrate AWS Lambda to an API Gateway method.
+    const productsFileImport = api.root.addResource('import'); // create "/import" route
+
+    const productsFileImportIntegration = new LambdaIntegration(
+      importProductsFileLambda
+    );
+
+    productsFileImport.addMethod('GET', productsFileImportIntegration, {
+      apiKeyRequired: false,
+    });
+
+    // new cdk.CfnOutput(this, 'API Key ID', {
+    //   value: apiKey.keyId,
     // });
   }
 }
---
+++ import-service/resources/handlers/importProductsFile.ts
@@ -0,0 +1,5 @@
+import { HEADERS } from '../constants';
+
+export async function importProductsFile({ fileName }: { fileName: string }) {
+  console.log('importProductsFile ', `uploaded/${fileName}`);
+}
