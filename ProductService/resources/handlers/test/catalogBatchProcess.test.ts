09bc829cbacc65602fba368875930c61c6ee980d
vasily <vasilymishanin@gmail.com>
2023-12-11 18:24:14 +0600

    test: update test for catalogBatchProcess

--- ProductService/resources/handlers/test/catalogBatchProcess.test.ts
+++ ProductService/resources/handlers/test/catalogBatchProcess.test.ts
@@ -1,9 +1,19 @@
 import { SQSEvent, SQSRecord } from 'aws-lambda';
 import { handler } from '../catalogBatchProcess';
 import { createProduct } from '../products/add-one';
+import {
+  SNSClient,
+  PublishCommand,
+  PublishCommandOutput,
+} from '@aws-sdk/client-sns';
 
 jest.mock('../products/add-one', () => ({
   createProduct: jest.fn(),
+}));
+
+jest.mock('@aws-sdk/client-sns', () => ({
+  SNSClient: jest.fn(),
+  PublishCommand: jest.fn(),
 }));
 
 describe('SQS Handler', () => {
@@ -11,12 +21,12 @@ describe('SQS Handler', () => {
     jest.clearAllMocks();
   });
 
-  it('should process SQS records', async () => {
+  it('should process SQS records and publish messages to SNS', async () => {
     const records: SQSRecord[] = [
       {
         messageId: '1',
         receiptHandle: 'handle1',
-        body: 'record1',
+        body: JSON.stringify('record1'),
         md5OfBody: 'md5hash1', // Add this line
         attributes: {
           ApproximateReceiveCount: '1',
@@ -32,7 +42,7 @@ describe('SQS Handler', () => {
       {
         messageId: '2',
         receiptHandle: 'handle2',
-        body: 'record2',
+        body: JSON.stringify('record2'),
         md5OfBody: 'md5hash2', // Add this line
         attributes: {
           ApproximateReceiveCount: '2',
@@ -51,10 +61,43 @@ describe('SQS Handler', () => {
       Records: records,
     };
 
+    // Mock SNSClient.send method
+    const mockSNSClientSend = jest.fn();
+    (SNSClient as jest.Mock).mockImplementationOnce(() => ({
+      send: mockSNSClientSend,
+    }));
+
+    // Mock the response from the SNS publish command
+    const mockPublishResponse: PublishCommandOutput = {
+      MessageId: 'mocked-message-id',
+      $metadata: {},
+    };
+    mockSNSClientSend.mockResolvedValueOnce(mockPublishResponse);
+
     await handler(event);
 
     // Check if createProduct is called with the correct records
-    expect(createProduct).toHaveBeenCalledWith('record1');
-    expect(createProduct).toHaveBeenCalledWith('record2');
+    expect(createProduct).toHaveBeenCalledWith(JSON.stringify('record1'));
+    expect(createProduct).toHaveBeenCalledWith(JSON.stringify('record2'));
+
+    // Check if SNSClient.send is called with the expected PublishCommand
+    expect(mockSNSClientSend).toHaveBeenCalledWith(
+      new PublishCommand({
+        TopicArn:
+          'arn:aws:sns:us-east-1:230091350506:ProductServiceStack-CreateProductTopicE4CD9217-d8IDnTwBjXKR',
+        Message:
+          '{"message":"New product(s) created: record1","productTitle":"record1"}',
+        Subject: 'Product Creation Event',
+      })
+    );
+    expect(mockSNSClientSend).toHaveBeenCalledWith(
+      new PublishCommand({
+        TopicArn:
+          'arn:aws:sns:us-east-1:230091350506:ProductServiceStack-CreateProductTopicE4CD9217-d8IDnTwBjXKR',
+        Message:
+          '{"message":"New product(s) created: record2","productTitle":"record2"}',
+        Subject: 'Product Creation Event',
+      })
+    );
   });
 });
